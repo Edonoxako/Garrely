b80d21d8e57df5e1806ed8eb6103759289fb024e
Senya <suzdalnicky@ktsstudio.ru>
2018-04-13 15:11:01 +0300

    Styling

--- app/src/main/java/live/senya/garrely/ui/main/view/MainActivity.kt
+++ app/src/main/java/live/senya/garrely/ui/main/view/MainActivity.kt
@@ -1,15 +1,65 @@
 package live.senya.garrely.ui.main.view
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
 import com.arellomobile.mvp.MvpAppCompatActivity
-import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.presenter.InjectPresenter
+import com.arellomobile.mvp.presenter.ProvidePresenter
+import kotlinx.android.synthetic.main.activity_main.*
 import live.senya.garrely.R
+import live.senya.garrely.di.Scopes
+import live.senya.garrely.entity.Photo
+import live.senya.garrely.ui.common.PaddingItemDecoration
+import live.senya.garrely.ui.photogrid.presenter.PhotoGridPresenter
+import live.senya.garrely.ui.photogrid.view.PhotoAdapter
+import live.senya.garrely.ui.photogrid.view.PhotoGridView
+import toothpick.Toothpick
 
-class MainActivity : MvpAppCompatActivity(), MvpView {
+class MainActivity : MvpAppCompatActivity(), PhotoGridView {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initList()
+        setSupportActionBar(toolBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    @InjectPresenter
+    lateinit var presenter: PhotoGridPresenter
+
+    private val gridSpanSize: Int
+        get() = when (resources.configuration.orientation) {
+            Configuration.ORIENTATION_LANDSCAPE -> 3
+            Configuration.ORIENTATION_PORTRAIT -> 2
+            else -> 2
+        }
+
+    private val photoAdapter: PhotoAdapter by lazy {
+        PhotoAdapter(presenter::openPhotoPager)
+    }
+
+    override fun showPhotos(photos: List<Photo>) {
+        photoAdapter.setItems(photos)
+    }
+
+    @ProvidePresenter
+    fun providePresenter(): PhotoGridPresenter {
+        return Toothpick.openScope(Scopes.APP_SCOPE)
+                .getInstance(PhotoGridPresenter::class.java)
+    }
+
+    private fun initList() = with(recyclerView) {
+        layoutManager = GridLayoutManager(this@MainActivity, gridSpanSize)
+        adapter = photoAdapter
+        setHasFixedSize(true)
+        PaddingItemDecoration(
+                resources.getDimensionPixelSize(R.dimen.photo_grid_item_padding),
+                gridSpanSize
+        )
+                .let(::addItemDecoration)
     }
 }
